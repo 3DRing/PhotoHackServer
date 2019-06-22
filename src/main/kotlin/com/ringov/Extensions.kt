9b967a776382c49d521eb3369b64a79c43dbb790
Ivan Antsiferov <antsiferov.ivan@gmail.com>
2019-06-22 21:10:32 +0300

    Fix kotlin version.

--- src/main/kotlin/com/ringov/Extensions.kt
+++ src/main/kotlin/com/ringov/Extensions.kt
@@ -1,9 +1,15 @@
 package com.ringov
 
 import java.awt.Rectangle
+import java.util.*
 
 val Rectangle.xRight: Int
     get() = x + width
 val Rectangle.yBottom: Int
     get() = y + height
 
+
+fun <T> List<T>.random(): T {
+    val index = Random().nextInt(size)
+    return get(index)
+}
