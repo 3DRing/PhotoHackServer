c9492a3ac550ee9e2b9784ba738643ac41da1b0b
Sergey Koltsov <ringov@yandex-team.ru>
2019-06-22 18:11:46 +0300

    Adding gradient.

--- src/main/kotlin/com/ringov/ImageModifier.kt
+++ src/main/kotlin/com/ringov/ImageModifier.kt
@@ -1,6 +1,7 @@
 package com.ringov
 
 import java.awt.Color
+import java.awt.GradientPaint
 import java.awt.Graphics2D
 import java.awt.Rectangle
 import java.io.File
@@ -21,13 +22,23 @@ class ImageModifier {
     private fun getRectangle(image: BufferedImage): Rectangle = Rectangle(0, 0, image.width, image.height)
 
     private fun draw(graphics: Graphics2D, rectangle: Rectangle) {
-        val blackOpacityColor = Color(0, 0, 0, 150)
+        val blackOpacityColor = Color(0, 0, 0, 90)
+        drawForegroundGradient(graphics, rectangle, blackOpacityColor)
 
+    }
+
+    private fun drawForegroundGradient(graphics: Graphics2D, rectangle: Rectangle, color: Color) {
         val bottomRectLine = 0.4
         val bottomRect = Rectangle(rectangle.x, (rectangle.height - (rectangle.height * bottomRectLine)).toInt(),
                 rectangle.width, (rectangle.height * bottomRectLine).toInt())
-        graphics.fillColoredRect(blackOpacityColor, bottomRect)
+        val gradientRect = Rectangle(bottomRect.x, bottomRect.y, bottomRect.x, rectangle.height)
+        graphics.fillGradientRect(buildLinearGradient(gradientRect, color), bottomRect)
     }
+
+    private fun buildLinearGradient(rect: Rectangle, color: Color) =
+            GradientPaint(rect.x.toFloat(), rect.y.toFloat(),
+                    Color(color.red, color.green, color.blue, 0), rect.x.toFloat(),
+                    rect.height.toFloat(), color)
 
     private fun getGraphic(image: BufferedImage): Graphics2D = image.createGraphics()
 
@@ -37,6 +48,13 @@ class ImageModifier {
         fun writeToFile(file: File) {
             ImageIO.write(image, file.extension, file)
         }
+    }
+
+    private fun Graphics2D.fillGradientRect(gradient: GradientPaint, rect: Rectangle) {
+        val paintTmp = this.paint
+        this.paint = gradient
+        fillRect(rect.x, rect.y, rect.width, rect.height)
+        this.paint = paintTmp
     }
 
     private fun Graphics2D.fillColoredRect(color: Color, rect: Rectangle) {
