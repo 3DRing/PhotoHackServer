5f494d05274354992e752e2dd1594c7f881654e1
Sergey Koltsov <ringov@yandex-team.ru>
2019-06-23 07:16:30 +0300

    Removing redundant generification.

--- src/main/kotlin/com/ringov/generator/Element.kt
+++ src/main/kotlin/com/ringov/generator/Element.kt
@@ -3,11 +3,11 @@ package com.ringov.generator
 import com.ringov.random
 import java.util.regex.Pattern
 
-open class Element<T>(private val patterns: List<T>) {
+open class Element(private val patterns: List<String>) {
     companion object {
-        val WILD_CARD = Pattern.compile("\\[(.*?)]")
+        private val WILD_CARD = Pattern.compile("\\[(.*?)]")
 
-        fun matchesList(sourceString: String, wildcard: Pattern): List<String> {
+        private fun matchesList(sourceString: String, wildcard: Pattern): List<String> {
             val matcher = wildcard.matcher(sourceString)
             val list = ArrayList<String>()
             while (matcher.find()) {
@@ -17,27 +17,32 @@ open class Element<T>(private val patterns: List<T>) {
         }
     }
 
-    constructor(pattern: T) : this(listOf(pattern))
+    constructor(pattern: String) : this(listOf(pattern))
 
-    private val subElement = HashMap<String, Element<*>>()
+    private val subElement = HashMap<String, MutableList<Element>>()
 
-    fun generate(toStringMapper: (T) -> String): String {
-        var result = toStringMapper(patterns.random())
+    fun generate(): String {
+        var result = patterns.random()
         val wildcards = matchesList(result, WILD_CARD)
         for (w in wildcards) {
-            val e = subElement[w.trimBraces()]
-            if (e != null) {
-                val randomElement = e.generate()
-                result = result.replace(w, randomElement)
+            val list = subElement[w.trimBraces()]
+            if (list != null) {
+                val randomElement = list.random()
+                val e = randomElement.generate()
+                result = result.replace(w, e)
             }
         }
         return result
     }
 
-    fun generate(): String = generate { it.toString() }
-
-    fun addSub(key: String, element: Element<*>) {
-        subElement[key] = element
+    fun addSub(key: String, element: Element) {
+        val list = subElement[key]
+        if (list == null) {
+            subElement[key] = mutableListOf(element)
+        } else {
+            list.add(element)
+            subElement[key] = list
+        }
     }
 
     private fun String.trimBraces() = this.subSequence(1, this.length - 1)
